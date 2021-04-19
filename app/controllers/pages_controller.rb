9b400feb0506b7ed81726c7477673bc1dc7e9244
hienviluong125 <hienviluong125@gmail.com>
2021-04-19 16:16:17 +0700

    Show today's currencies rate in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home
-
+    symbols = %w[AUD USD VND SGD JPY MMK SGD TWD PHP KPW KRW MYR INR HKD CNY]
+    response = HTTParty.get("http://api.exchangeratesapi.io/v1/latest?access_key=#{ENV['EXCHANGE_RATE_API_KEY']}&symbols=#{symbols.join(',')}")
+    @rates = []
+    @base_on = nil
+    if response["success"]
+      @rates = response["rates"]
+      @base_on = response["base"]
+    end
   end
 end
