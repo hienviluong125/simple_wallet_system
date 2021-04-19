3c1ba2dc46744b0f38ca969491e3ff2e05a251f7
hienviluong125 <hienviluong125@gmail.com>
2021-04-19 15:36:12 +0700

    notify via telegram when admin handle transaction

--- app/controllers/dashboard/transactions_controller.rb
+++ app/controllers/dashboard/transactions_controller.rb
@@ -1,4 +1,6 @@
 class Dashboard::TransactionsController < Dashboard::BaseController
+  include ActionView::Helpers::NumberHelper
+
   def index
     @transactions = Transaction.all.order(created_at: :desc)
   end
@@ -9,9 +11,22 @@ class Dashboard::TransactionsController < Dashboard::BaseController
     return redirect_to dashboard_transactions_path, alert: 'Cannot perform this action' if @transaction.status != 'pending'
 
     if @transaction.update(status: params[:status])
+      notify_via_telegram(@transaction) if @transaction.status == "handled"
       redirect_to dashboard_transactions_path, notice: 'Transaction updated succesfully'
     else
       redirect_to dashboard_transactions_path, alert: 'Failure to update transaction'
     end
+  end
+
+  private
+
+  def notify_via_telegram(transaction)
+    text = if transaction.transaction_type == 'withdraw'
+             "#{transaction.user.email} withdrew #{number_to_currency(transaction.amount)}"
+           else
+             "#{transaction.user.email} deposited #{number_to_currency(transaction.amount)}"
+           end
+
+    TelegramNotificationJob.perform_later(text)
   end
 end
